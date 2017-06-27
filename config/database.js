a380ef9d41475b13e88f4bc161805182f670dafb
perezfjv <perezfjv@gmail.com>
2017-06-27 13:13:35 +0200

    Proyecto JS Mean Empleados

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+
+	//'url' : 'mongodb://prueba:12345@ds037824.mongolab.com:37824/afiliado' 
+
+	'url' : 'mongodb://prueba:123456@ds135252.mlab.com:35252/afiliado'
+
+	 //mongo ds037824.mongolab.com:37824/afiliado -u prueba -p 12345
+
+};
